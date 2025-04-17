f4d476c7f37a1abab0c5087f012ec1d2b26675a3
Rexhark <105967519+Rexhark@users.noreply.github.com>
2025-04-17 17:38:03 +0800

    initial commit to a new version of this ordering app

--- resources/views/components/auth-footer.blade.php
+++
@@ -1,3 +0,0 @@
-  <div class="simple-footer">
-      Copyright &copy; Fitrah Ramadhan 2022
-  </div>
--- resources/views/components/auth-header.blade.php
+++
@@ -1,3 +0,0 @@
- <div class="login-brand">
-    <img src="{{ asset('img/food.svg') }}" alt="logo" width="100" class="shadow-dark rounded-circle">
- </div>
